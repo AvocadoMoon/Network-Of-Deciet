ec083e4cf237ae946837b51af10cd8f29e86244c
Zek <zek@pop-os.localdomain>
2022-11-10 19:48:32 -0500

    Finished tweet scraper

--- tweetScrape.py
+++ tweetScrape.py
@@ -4,10 +4,12 @@ import requests
 import pandas as pd
 import json
 import csv
+import os
+from datetime import date
 
-#Initialize Twitter Scraper
-
-
+###############################
+## Get Keys and authenticate ##
+###############################
 config = configparser.ConfigParser()
 config.read("config.ini")
 
@@ -19,81 +21,81 @@ access_token = config["twitter"]["access_token"]
 
 bearer_token = config["twitter"]["bearer_token"]
 
-
-
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 client = tweepy.Client(auth)
 
-tweet_fields = ["lang,created_at,id,author_id"]
-place_fields = [""]
-user_fields = ["location"]
-expansions = ["author_id"]
 
-csvWriter = csv.writer(open('11-10-2022_Tweets/tweets.csv', 'w'), delimiter=",")
+##########################
+## Tweet Scraping Logic ##
+##########################
+cwd = os.getcwd()
+directory = f"{cwd}/ScrapedTweets/{date.today()}_Tweets"
+path = directory + "/tweets.csv"
 
-def getCountryCode(self, ip):
-    endpoint = f'https://ipinfo.io/{ip}/json'
-    response = requests.get(endpoint, verify = True)
-
-    if response.status_code != 200:
-        return 'Status:', response.status_code, 'Problem with the request. Exiting.'
-        exit()
-
-    data = response.json()
-    return data['country']
-
-df = pd.DataFrame(columns=["Time", "Tweet", "TweetID", "AuthorID", "Location"])
-csvWriter.writerow(["Time", "Tweet", "TweetID", "AuthorID", "Location"])
-
+csvWriter = None
+columns = ["Time", "Tweet", "TweetID", "AuthorID", "Username", "Location"]
+if (not(os.path.exists(path))):
+	os.mkdir(directory)
+	csvWriter = csv.writer(open(path, 'w'), delimiter=",")
+	csvWriter.writerow(columns)
+if (os.path.exists(path)):
+	csvWriter = csv.writer(open(path, 'a'), delimiter=",")
 
 class MyStream(tweepy.StreamingClient):
 
-    limit = 20000
+    limit = 15000
     nTweets = 0
     def on_connect(self):
         print("Connected!")
-
+	
+	#Get only original english tweets then add them to the CSV file
+	#Original means no retweets
     def on_data(self, raw_data):
         data = json.loads(raw_data.decode())
-        if (nTweets == limit):
-			print("Disconnecting")
-			self.disconnect()
+        is_retweet = False if "referenced_tweets" not in data["data"] else data["data"]["referenced_tweets"][0]["type"] == "retweeted"
+        is_english = data["data"]["lang"] == 'en'
+        has_location = "location" in data["includes"]["users"][0]
         
-        elif data["data"]["lang"] == 'en':
+        if (self.nTweets == self.limit):
+            print("Disconnecting")
+            self.disconnect()
+        
+        elif is_english and has_location and not is_retweet:
             #make sure that the location taken is from the tweeter
+            #print(data)
             assert(data["includes"]["users"][0]["id"] == data["data"]["author_id"])
-            print(data["includes"]["users"][0])
-    
-    #Get only english tweets, and then 
-    # def on_tweet(self, tweet):
-        #print(tweet.data)
-        # print(api.get_user(tweet.author_id))
-        # if(len(df.index) > self.limit):
-            # print("Disconnected")
-            # self.disconnect()
-        # elif tweet.lang == "en": #and tweet.geo.country_code == "US":
-            # print(tweet.data)
-            # twit = [tweet.created_at, tweet.text, tweet.id, tweet.author_id]
-
-            #add new row to end of data base
-            # df.loc[len(df.index)] = twit
-            # print(len(df.index))
+            row = [data["data"]["created_at"], data["data"]["text"], data["data"]["id"], data["data"]["author_id"], data["includes"]["users"][0]["username"], data["includes"]["users"][0]["location"]]
+            csvWriter.writerow(row)
+            self.nTweets += 1
 
 
-    
+###################################
+## Use Of Twitter Scraping Logic ##
+###################################
+if __name__ == "__main__":
+	tweet_fields, user_fields, expansions = ["lang,created_at,id,author_id,referenced_tweets"],["location"], ["author_id"] 
 
-stream = MyStream(bearer_token)
-stream.sample(tweet_fields=tweet_fields,place_fields=place_fields, user_fields=user_fields, expansions=expansions)
-
-#df.info()
-
-#Due to difficulty of randomly sampling tweets from certain dates, using Tweet ID, streaming random Tweets to get current sentiment
+	stream = MyStream(bearer_token)
+	stream.sample(tweet_fields=tweet_fields,user_fields=user_fields, expansions=expansions)
 
 
-#tweet data structure https://developer.twitter.com/en/docs/twitter-api/tweets/likes/api-reference/get-users-id-liked_tweets
-#tweet look up https://docs.tweepy.org/en/stable/client.html#tweets
+#################
+## Limitations ##
+#################
+# Can not confirm location due to the location being grabed from users bio which may be false
+
+# Would be better to sample random tweets within time frame but not possible unless researcher
+
+#Tweets handle is given, but when going back for reference they may be deleted
+
+################
+## References ##
+################
+
+#https://developer.twitter.com/en/docs/twitter-api/tweets/likes/api-reference/get-users-id-liked_tweets
+#https://docs.tweepy.org/en/stable/client.html#tweets
 #https://developer.twitter.com/apitools/api?endpoint=%2F2%2Ftweets%2Fsample%2Fstream&method=get
 #https://developer.twitter.com/en/docs/twitter-api/tweets/volume-streams/api-reference/get-tweets-sample-stream#tab1
